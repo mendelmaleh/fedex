4ccdc1fa88f4f18fd0749ce64ef55ba03304cc26
Mendel E <mendelmaleh@gmail.com>
2022-08-30 03:59:48 -0400

    Abstract api client

---
+++ client.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+)
+
+type Client struct {
+	Config Config
+	OAuth  OAuthResponse
+}
+
+type Config struct {
+	ID     string
+	Secret string
+	URL    string
+}
+
+func (c *Client) RefreshToken() error {
+	v := url.Values{}
+	v.Set("grant_type", "client_credentials")
+	v.Set("client_id", c.Config.ID)
+	v.Set("client_secret", c.Config.Secret)
+
+	resp, err := http.PostForm(c.Config.URL+"/oauth/token", v)
+	if err != nil {
+		return err
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&c.OAuth); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) Track(number string) (tracking TrackResponse, err error) {
+	if c.OAuth == (OAuthResponse{}) {
+		if err = c.RefreshToken(); err != nil {
+			return
+		}
+	}
+
+	payload := TrackingRequest{
+		IncludeDetailedScans: true,
+		TrackingInfo: []TrackingInfoRequest{
+			{
+				TrackingNumberInfo: TrackingNumberInfoRequest{
+					TrackingNumber: number,
+				},
+			},
+		},
+	}
+
+	var buf bytes.Buffer
+	if err = json.NewEncoder(&buf).Encode(payload); err != nil {
+		return
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, c.Config.URL+"/track/v1/trackingnumbers", &buf)
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-locale", "en_US")
+	req.Header.Set("Authorization", "Bearer "+c.OAuth.AccessToken)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&tracking); err != nil {
+		return
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/kr/pretty"
 	"github.com/pelletier/go-toml/v2"
 )
 
-type Config struct {
+type TomlConfig struct {
 	Fedex struct {
-		ID             string
-		Secret         string
-		URL            string
+		Config
 		TrackingNumber string
 	}
 }
@@ -29,63 +22,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var config Config
+	var config TomlConfig
 	if err = toml.Unmarshal(data, &config); err != nil {
 		log.Fatal(err)
 	}
 
-	// oauth token
-	v := url.Values{}
-	v.Set("grant_type", "client_credentials")
-	v.Set("client_id", config.Fedex.ID)
-	v.Set("client_secret", config.Fedex.Secret)
+	cl := Client{Config: config.Fedex.Config}
 
-	resp, err := http.PostForm(config.Fedex.URL+"/oauth/token", v)
+	tracking, err := cl.Track(config.Fedex.TrackingNumber)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	var oauth OAuthResponse
-	if err := json.NewDecoder(resp.Body).Decode(&oauth); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(oauth)
-
-	// tracking
-	payload := TrackingRequest{
-		IncludeDetailedScans: true,
-		TrackingInfo: []TrackingInfoRequest{
-			{
-				TrackingNumberInfo: TrackingNumberInfoRequest{
-					TrackingNumber: config.Fedex.TrackingNumber,
-				},
-			},
-		},
-	}
-
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(payload); err != nil {
-		log.Fatal(err)
-	}
-
-	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodPost, config.Fedex.URL+"/track/v1/trackingnumbers", &buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("X-locale", "en_US")
-	req.Header.Set("Authorization", "Bearer "+oauth.AccessToken)
-
-	resp, err = client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var tracking TrackResponse
-	if err := json.NewDecoder(resp.Body).Decode(&tracking); err != nil {
 		log.Fatal(err)
 	}
 
